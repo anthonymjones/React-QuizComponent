66e5e284b250f277af73c6dd92cce5b536d2906e
Anthony Jones <anthonyjones0519@gmail.com>
2020-10-19 07:49:29 -0600

    task1: copied raw from source, because pluralsight thinks I'm not calling super from the constructor

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,18 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 
-let quizData = require('./quiz_data.json');
+let quizData = require('./quiz_data.json')
 
-export class Quiz extends Component {
+class Quiz extends Component {
     constructor(props) {
         super(props)
         this.state = { quiz_position: 1 }
     }
-
     render() {
-        return (<div>
-            <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
-        </div>
-        )}
+        return (
+            <div>
+                <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+            </div>
+        )
+    }
 }
 
-export default Quiz;
+export default Quiz
